40d61e0e48200bdd5238c2504d04703e6f5c3729
ashrithjacob <ashrithjacob@gmail.com>
2023-05-02 18:34:48 +0200

    update novideo

--- keyframe/novideo.py
+++ keyframe/novideo.py
@@ -1,18 +1,14 @@
 import cv2
 import numpy as np
-import torch
-import torchvision
 import matplotlib.pyplot as plt
 import os
 import sys
 import time
-from torchvision.transforms import ToTensor
 from MS_SSIM_L1_loss import MS_SSIM_L1_LOSS
-from processing import resume, get_latest_epoch
-from model import Autoencoder
+from processing import Helper
 
 
-class NoVideo:
+class Video:
     def __init__(self, path, per_second_acquisition=5, threshold=60):
         """
         class to extract frames from a video
@@ -29,7 +25,6 @@ class NoVideo:
         self.path = path
         self.per_second_acquisition = per_second_acquisition
         self.threshold = threshold
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def get_generators(self):
         """
@@ -44,17 +39,38 @@ class NoVideo:
         frames_generator = None
         return deblurred_frames_generator, key_frames_generator
 
-    def frame_rate(self, video):
+    def save(self,directory, type = 'blur'):
         """
-        Get the frame rate of a video.
+        Save the frames of the video in a directory.
 
         Parameters:
-        video (cv2.VideoCapture): A cv2.VideoCapture object representing the video.
+        directory (str): The path to the directory where the frames are to be saved.
+        type (str): The type of frames to be saved. 'blur' for deblurred frames and 'key' for key frames.
 
         Returns:
-        float: The frame rate of the video.
+        None
         """
-        return video.get(cv2.CAP_PROP_FPS)
+        Helper.mkdir(directory)
+        deblurred_frames_generator,key_frames_generator =self.get_generators()
+        if type == 'blur':
+            self.save_frames(directory, deblurred_frames_generator)
+        elif type == 'key':
+            self.save_frames(directory, key_frames_generator)
+
+    def save_frames(self, directory, frames_generator):
+        """
+        Save the frames of the video in a directory.
+
+        Parameters:
+        directory (str): The path to the directory where the frames are to be saved.
+        frames_generator (generator): A generator object that yields the frames of the video.
+
+        Returns:
+        None
+        """
+        for i, frame in enumerate(frames_generator):
+            cv2.imwrite(os.path.join(directory, f"s_{i}.png"), frame)
+        print(f"Saved {i+1} number of frames")
 
     def extract(self, video):
         """"
@@ -68,7 +84,7 @@ class NoVideo:
         generator: A generator object that yields the frames of the video.
         """
         current_frame = 0
-        fps = self.frame_rate(video)
+        fps = Helper.frame_rate(video)
         print(f"FPS: {fps}")
         collect_every = fps // self.per_second_acquisition #Raiseerror if fps < per_second_acquisition
         while True:
@@ -81,18 +97,6 @@ class NoVideo:
                 break
         video.release()
         cv2.destroyAllWindows()
-
-    def bgr_to_rgb(self, image):
-        """
-        Convert a BGR image to RGB format.
-
-        Parameters:
-        image (numpy.ndarray): A 3-dimensional numpy array representing the BGR image.
-
-        Returns:
-        numpy.ndarray: A 3-dimensional numpy array representing the RGB image.
-        """
-        return image[..., ::-1]
 
     def deblur(self, frames_generator):
         """
@@ -125,73 +129,18 @@ class NoVideo:
         Returns:
         generator: A generator object that yields the key frames of the video.
         """
-        model = self.load_model()
+        model = Helper.load_model()
         diff = MS_SSIM_L1_LOSS()
         try:
             frame1 = next(deblurred_frames_generator)
             yield frame1
             while True:
                 frame2 = next(deblurred_frames_generator)
-                if diff(self.transform(frame1), self.transform(frame2)).item() > self.threshold:
+                if diff(Helper.transform(frame1), Helper.transform(frame2)).item() > self.threshold:
                     yield frame2
                 frame1 = frame2
         except StopIteration:
             print(f"Key frames generated")
-
-    def save(self,directory, type = 'blur'):
-        """
-        Save the frames of the video in a directory.
-
-        Parameters:
-        directory (str): The path to the directory where the frames are to be saved.
-        type (str): The type of frames to be saved. 'blur' for deblurred frames and 'key' for key frames.
-
-        Returns:
-        None
-        """
-        self.mkdir(directory)
-        deblurred_frames_generator,key_frames_generator =self.get_generators()
-        if type == 'blur':
-            self.save_frames(directory, deblurred_frames_generator)
-        elif type == 'key':
-            self.save_frames(directory, key_frames_generator)
-
-    def save_frames(self, directory, frames_generator):
-        """
-        Save the frames of the video in a directory.
-
-        Parameters:
-        directory (str): The path to the directory where the frames are to be saved.
-        frames_generator (generator): A generator object that yields the frames of the video.
-
-        Returns:
-        None
-        """
-        for i, frame in enumerate(frames_generator):
-            cv2.imwrite(os.path.join(directory, f"s_{i}.png"), frame)
-        print(f"Saved {i} number of frames")
-
-    def load_model(self):
-        """
-        Load the model for inference.
-        """
-        model = Autoencoder().to(self.device)
-        checkpoint_dir = "./checkpoints"
-        loading_epoch = get_latest_epoch(checkpoint_dir)
-        if loading_epoch != 0:
-            resume(model, f"{str(checkpoint_dir)}/MS_SSIM_L1-epoch-{str(loading_epoch)}.pth")
-            print(f"start inference from epoch {str(loading_epoch)}")
-        else:
-            raise Exception("No checkpoint found, please upload checkpoint for inference")
-        model.eval()
-        return model
-
-    def transform(self, frame):
-        frame = self.bgr_to_rgb(frame)
-        frame = ToTensor()(frame.copy())
-        frame = torch.unsqueeze(frame, axis=0)
-        frame = frame.to(self.device)
-        return frame
 
     def display(self, type = 'blur'):
         """
@@ -204,24 +153,11 @@ class NoVideo:
             frames_generator = key_frames_generator
         try:
             for frame in frames_generator:
-                plt.imshow(self.bgr_to_rgb(frame))
+                plt.imshow(Helper.bgr_to_rgb(frame))
                 plt.show()
         except KeyboardInterrupt:
             print("Exiting...")
             sys.exit()
-
-    def mkdir(self, directory):
-        """
-        Create a directory if it doesn't exist.
-
-        Parameters:
-        directory (str): The path to the directory to be created.
-        """
-        try:
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-        except OSError:
-            print("Error: Creating directory for frame storage:", str(directory))
 
     def __len__(self, type = 'blur'):
         """
@@ -238,7 +174,7 @@ class NoVideo:
 
 if __name__ == "__main__":
     start_time = time.time()
-    VID = 'V0'
+    VID = 'V1'
     video = Video(path="../VIDEOS/" + VID +".mp4", per_second_acquisition=5)
     print(f'number of deblurred frames: {len(video)}')
     video.save(directory="../VIDEOS/deblurred/" + VID + "/", type='blur')
